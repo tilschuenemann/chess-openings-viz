4425ce716273ac87bbaf2119750ea43fb8bd7d3f
ga87piy <leon.kiesgen@tum.de>
2022-06-03 16:36:23 +0200

    fixed alignment and centering

--- viz/visTree.js
+++ viz/visTree.js
@@ -3,14 +3,12 @@ const nodeHeight = 90
 const highlightSize = 2
 
 const maxNodesPerRow = 600
-const maxNodesPerCol = 30
 
 const imageDims = nodeWidth - highlightSize
 
 
 const margin = { top: 20, right: 120, bottom: 20, left: 120 }
-const width = nodeWidth * maxNodesPerRow - margin.right - margin.left
-const height = nodeHeight * maxNodesPerCol - margin.top - margin.bottom
+
 
 fetch("../tree.json")
     .then(response => {
@@ -20,16 +18,7 @@ fetch("../tree.json")
 
         const root = data[0].children[7];
         root.x0 = 0;
-        root.y0 = width / 2;
-
-        // setup svg 
-        const svg = d3.select("body").append("svg")
-            .attr("width", width + margin.right + margin.left)
-            .attr("height", height + margin.top + margin.bottom)
-            .attr("id", "svg")
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+        root.y0 = 0;//width / 2;
 
         // generate tree layeout depending on node size
         var tree = d3.layout.tree()
@@ -41,17 +30,52 @@ fetch("../tree.json")
             links = tree.links(nodes);
         console.log(nodes)
 
+        var x_max = d3.max(nodes, function(d) {
+            return +d.x;
+         })
+        var x_min = d3.min(nodes, function(d) {
+           return +d.x;
+         });
+
+        var y_max = d3.max(nodes, function(d) {
+            return +d.y;
+         })
+        var y_min = d3.min(nodes, function(d) {
+           return +d.y;
+         });
+
+        const boundingbox = {x_max: x_max, x_min:x_min,y_max:y_max,y_min:y_min }
+        
+        console.log(boundingbox)
+        
+        const width = (boundingbox.x_max-boundingbox.x_min) + nodeWidth // - margin.right - margin.left
+        const height = (boundingbox.y_max-boundingbox.y_min) + nodeHeight
+        const maxNodesPerCol =  d3.max(nodes, function(d) {
+            return +d.depth+1;
+          });
+
+
+
         var line = d3.link(d3.curveBumpY)
             .x(function (d) { return d.x; })
             .y(function (d) { return d.y; })
         console.log(line)
+
+        // setup svg 
+        const svg = d3.select("body").append("svg")
+            .attr("width", width + margin.right + margin.left)
+            .attr("height", height + margin.top + margin.bottom)
+            .attr("id", "svg")
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 
         svg.selectAll("rect")
             .data([...Array(maxNodesPerCol).keys()])
             .enter().append("rect")
             .attr("x", 0)
             .attr("y", function (d) { return d * nodeHeight; })
-            .attr("width", width)
+            .attr("width", width+nodeWidth)
             .attr("height", nodeHeight)
             .attr("fill", function (d) { return (d % 2 ? "#333333" : "#eeeeee") })
             //.attr('transform', (d) => `translate(0,0)`)
@@ -66,7 +90,7 @@ fetch("../tree.json")
             .data(links)
             .enter().append("path")
             .attr("d", line)
-            .attr('transform', d => `translate(${width / 2}, ${2 * nodeHeight / 4})`)
+            .attr('transform', d => `translate(${Math.abs(boundingbox.x_min)+nodeWidth/2+nodeWidth/2}, ${2 * nodeHeight / 4})`)
             .style("fill", "none")
             .style("stroke", d => (d.source.color))
             .style("stroke-width", highlightSize + "px");
@@ -75,8 +99,9 @@ fetch("../tree.json")
             .data(nodes)
             .enter()
             .append('g')
-            .attr('transform', d => `translate(${d.x + width / 2}, ${d.y + nodeHeight / 4})`)
+            .attr('transform', d => `translate(${Math.abs(boundingbox.x_min)+nodeWidth/2+nodeWidth/2+d.x}, ${d.y + nodeHeight / 4})`)
             .style("opacity", d => (d.name == "P00" ? "0.5" : "1"))
+            .style("fill-opacity", d => (d.name == "P00" ? "0.5" : "1"))
 
         g.append('text')
             //.attr("transform", function (d) { return "translate(0,12)"; })
@@ -86,6 +111,7 @@ fetch("../tree.json")
             .attr("text-anchor", "middle")
             .attr("y", nodeHeight * 0.5)
             .style("visibility", d => (d.name == "P00" ? "hidden" : "visible"))
+            .style("fill-opacity", d => (d.name == "P00" ? "0" : "1"))
             .call(wrap, 50);
 
         g.append('rect')
@@ -95,6 +121,7 @@ fetch("../tree.json")
             .attr('transform', d => "translate(-" + nodeWidth / 2 + ",-" + highlightSize / 2 + ")")
             .attr("rx", "1")
             .style("opacity", d => (d.name == "P00" ? "0.5" : "1"))
+            .style("fill-opacity", d => (d.name == "P00" ? "0.5" : "1"))
 
         const board = g.append('image')
             .attr('xlink:href', d => "../" + d.path)
@@ -102,7 +129,9 @@ fetch("../tree.json")
             .attr('height', imageDims)
             .attr('loading', "lazy")
             .attr("transform", function (d) { return "translate(-" + imageDims / 2 + ",0)"; })
+            .style("fill-opacity", d => (d.name == "P00" ? "0.5" : "1"))
 
+            
         function wrap(text, width) {
             text.each(function () {
                 var text = d3.select(this),
